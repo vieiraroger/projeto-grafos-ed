4d456838432ed78afdcef6d76c58307fe6b58ed7
comp15 <comp15@lab.unesc.net>
2018-08-08 21:18:46 -0300

    Creation of getters, setters and properties of Users and Citys Class

--- GoHorseMvc/src/com/gohorse/database/model/Users.java
+++ GoHorseMvc/src/com/gohorse/database/model/Users.java
@@ -1,5 +1,41 @@
 package com.gohorse.database.model;
 
 public class Users {
+	
+	private static long userID;
+	
+	private String user;
+	
+	private String password;
+	
+	private String perfil;
+
+	public String getuser() {
+		return user;
+	}
+
+	public void setuser(String user) {
+		user = user;
+	}
+
+	public String getpassword() {
+		return password;
+	}
+
+	public void setpassword(String password) {
+		password = password;
+	}
+
+	public String getperfil() {
+		return perfil;
+	}
+
+	public void setperfil(String perfil) {
+		perfil = perfil;
+	}
+
+	public users() {
+	
+	}
 
 }
