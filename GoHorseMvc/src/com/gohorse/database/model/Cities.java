db3d93ad1f23950928a8d5b542ada7e0a619045d
Roger Vieira <rogeramorinvieiraa@gmail.com>
2018-08-09 19:47:42 -0300

    Update Cities.java

--- GoHorseMvc/src/com/gohorse/database/model/Cities.java
+++ GoHorseMvc/src/com/gohorse/database/model/Cities.java
@@ -2,5 +2,40 @@ package com.gohorse.database.model;
 
 public class Cities {
 	
+	private static long cityID;
+	
+	private String city;
+	
+	private String state;
+	
+	private String country;
+
+	public String getcity() {
+		return city;
+	}
+
+	public void setcity(String city) {
+		city = city;
+	}
+
+	public String getstate() {
+		return state;
+	}
+
+	public void setstate(String state) {
+		state = state;
+	}
+
+	public String getcountry() {
+		return country;
+	}
+
+	public void setcountry(String country) {
+		country = country;
+	}
+
+	public Cities() {
+	
+	}
 
 }
