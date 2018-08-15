a4800d0c213769a2928d6ba26efd6ebf1a5ae2fc
Bruno Firme <brunofirme.crema@hotmail.com>
2018-08-15 19:52:02 -0300

    start windows

--- GoHorseMvc/src/com/gohorse/view/LoginWindow.java
+++ GoHorseMvc/src/com/gohorse/view/LoginWindow.java
@@ -13,4 +13,25 @@ public class LoginWindow extends JDialog {
     private JButton btnConfirm;
     private JButton btnSair;
 
+
+    public vLogin() {
+
+        setSize(500,500);
+        setTitle("Titulo da Janela");
+        setLayout(null);
+        setResizable(false);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        
+        
+
+    }
+    
+    public static void main(String[] args) {
+        
+        new vLogin().setVisible(true);
+        
+        v
+
+    }
+
 }
