b82638dbdc8fe1e3036979dfdaf2879199041f5f
RomuloRPS <romulo.rps@hotmail.com>
2018-08-15 19:55:48 -0300

    I changed the class Student

--- GoHorseMvc/src/com/gohorse/database/model/Students.java
+++ GoHorseMvc/src/com/gohorse/database/model/Students.java
@@ -1,7 +1,145 @@
 package com.gohorse.database.model;
 
+import java.util.Date;
+
 public class Students {
 	
+	private int student_id;
+	private String student;
+	private Date birthdate;
+	private char sex;
+	private String phone;
+	private String cellphone;
+	private String email;
+	private String observacao;
+	private String endereco;
+	private String numero;
+	private String complemento;
+	private String bairro;
+	private String cidade;
+	private char[] estado;
+	private String cep;
 	
-
+	public int getStudent_id() {
+		return student_id;
+	}
+	public void setStudent_id(int student_id) {
+		this.student_id = student_id;
+	}
+	
+	
+	public String getStudent() {
+		return student;
+	}
+	public void setStudent(String student) {
+		this.student = student;
+	}
+	
+	
+	public Date getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+	
+	
+	public char getSex() {
+		return sex;
+	}
+	public void setSex(char sex) {
+		this.sex = sex;
+	}
+	
+	
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+	public String getCellphone() {
+		return cellphone;
+	}
+	public void setCellphone(String cellphone) {
+		this.cellphone = cellphone;
+	}
+	
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	public String getObservacao() {
+		return observacao;
+	}
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+	
+	
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+	
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	
+	public String getComplemento() {
+		return complemento;
+	}
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	
+	
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	
+	
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	
+	
+	public char[] getEstado() {
+		return estado;
+	}
+	public void setEstado(char[] estado) {
+		this.estado = estado;
+	}
+	
+	
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	
+	
+		
+	
 }
