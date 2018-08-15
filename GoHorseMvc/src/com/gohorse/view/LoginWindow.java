666a3ab894ada789fcacce735c5a34546c84dece
Jackson <jackson@localhost.localdomain>
2018-08-14 23:34:37 -0300

    Create the fields of login

--- GoHorseMvc/src/com/gohorse/view/LoginWindow.java
+++ GoHorseMvc/src/com/gohorse/view/LoginWindow.java
@@ -1,7 +1,15 @@
 package com.gohorse.view;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class LoginWindow extends JDialog {
+    
+    private JTextField txfUser;
+    private JTextField txfSenha;
+	private JButton btnConfirm;
 
 }
