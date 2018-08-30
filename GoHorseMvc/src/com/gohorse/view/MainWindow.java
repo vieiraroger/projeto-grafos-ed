fb50c74dd31ac504eb8d1a30d068ea541c6180a1
Bruno <brunofirme.crema@hotmail.com>
2018-08-30 20:49:34 -0300

    start db screens

--- GoHorseMvc/src/com/gohorse/view/MainWindow.java
+++ GoHorseMvc/src/com/gohorse/view/MainWindow.java
@@ -1,5 +1,6 @@
 package com.gohorse.view;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -9,14 +10,25 @@ import javax.swing.JMenuItem;
 import com.gohorse.database.model.Users;
 
 public class MainWindow extends JFrame {
-		
+	
+	
 	private JMenuBar menu ;
 	private JMenu mAlunos;
 	private JMenu mCidades;
-	private JMenuItem smCadastroAluno;
-	private JMenuItem smCadastroCidade;
-	private JMenuItem smListarAluno;
 	private JMenuItem smListarCidade;
+	private JMenuItem smListarUsuario;
+	private JMenuItem smListarAluno;
+	
+	// buttons for database manipulation
+	private JButton dCadastroCidade;
+	private JButton dCadastroUsuario;
+	private JButton dCadastroAluno;
+	private JButton dEditarCidade;
+	private JButton dEditarUsuario;
+	private JButton dEditarAluno;
+	private JButton dExcluirCidade;
+	private JButton dExcluirUsuario;
+	private JButton dExcluirAluno;
 	
 	public MainWindow (Users user) {
 		setSize(500,500);
@@ -29,13 +41,14 @@ public class MainWindow extends JFrame {
 	}
 	public void  CreateMenucomponents() {
 		 menu = new  JMenuBar();
+		 
 		 setJMenuBar(menu);
-		 mAlunos = new JMenu("Alunos");
-		 mCidades = new JMenu("Cidades");
-		 smCadastroAluno = new JMenuItem("Cadastrar");
-		 smCadastroCidade = new JMenuItem("Cadastrar");
-		 smListarAluno = new JMenuItem("Listar");
-		 smListarCidade = new JMenuItem("Listar");
+		 
+		 mAlunos 			= new JMenu("Alunos");
+		 mCidades			= new JMenu("Cidades");
+		 mUsuarios			= new JMenu("Usuarios");
+		 smListarAluno	    = new JMenuItem("Listar");
+		 smListarCidade     = new JMenuItem("Listar");
 		 
 		 menu.add(mAlunos);
 		 menu.add(mCidades);
@@ -46,6 +59,24 @@ public class MainWindow extends JFrame {
 		 mCidades.add(smCadastroCidade);
 		 mCidades.add(smListarCidade);
 	}
+	
+	public void CitiesWindow() {
+		
+		
+		
+	}
+	
+	
+	public void StudentsWindow() {
+		
+		
+	}
+	
+	public void UsersWindow() {
+		
+		
+	}
+	
 	public static void main(String[] args) {
 		new MainWindow().setVisible(true);
 	}
